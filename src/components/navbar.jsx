fcc6936b42626a563caf695adea142ed8a80f13c
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-05-20 22:10:32 +0700

    fix: font weight for sentifica

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -19,7 +19,7 @@ function Navbar() {
 
     return (
         <div className={`w-full h-18 bg-background flex justify-between items-center px-8 md:px-12 lg:px-24 xl:px-32 z-10 fixed`}>
-            <h1 className={`text-xl 2xl:text-2xl text-primary font-semibold`}>Sentifica</h1>
+            <h1 className={`text-xl 2xl:text-2xl text-primary font-bold`}>Sentifica</h1>
             <button className={`w-10 h-10 2xl:w-12 2xl:h-12 bg-primary rounded-md outline-none cursor-pointer`} onClick={() => toggleTheme()}>
                 <i className={`${theme === "light" ? "ri-moon-clear-line" : "ri-sun-line"} text-2xl 2xl:text-3xl text-white`}/>
             </button>
