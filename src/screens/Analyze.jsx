6d2ddeaa435dcbf4731bb4c8a8ebe0c19aa10700
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-05-20 23:47:42 +0700

    new page

---
+++ src/screens/Analyze.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
+function Analyze () {
+
+    const [value, setValue] = useState('')
+
+    function handleValueChange(event) {
+        setValue(event.target.value)
+    }
+
+    return (
+        <div className={`w-full md:max-h-[1024px] h-fit lg:h-screen px-8 md:px-12 lg:px-24 xl:px-32 py-20 flex flex-col items-center`}>
+            <div className={`w-full`}>
+                <Link to="/">
+                    <i className={`ri-arrow-left-s-line text-3xl text-text`}/>
+                </Link>
+            </div>
+            
+            <div className={`max-w-xl w-full flex flex-col space-y-2`}>
+                <h2 className={`text-lg 2xl:text-xl text-text font-bold`}>Type your text here, and let us analyze!</h2>
+                <textarea className={`w-full text-text resize-none border border-2 border-accent rounded-md p-3`} rows={5} onChange={handleValueChange}></textarea>
+                <div className={`flex justify-end`}>
+                    <button className={`w-fit h-fit px-3 2xl:px-5 py-1.5 2xl:py-2 bg-primary rounded-md space-x-2 text-white text-md xl:text-xl font-bold cursor-pointer`}>
+                        <span>Analyze</span>
+                        <i className={`ri-magic-fill`}/>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Analyze
