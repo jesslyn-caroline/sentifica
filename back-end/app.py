eaf7d7a7dcc53b63a2f2672138f5b599ca0fe78b
Jesslyn Caroline <158556535+jesslyn-caroline@users.noreply.github.com>
2025-05-23 15:01:45 +0700

    fix on main

--- back-end/app.py
+++ back-end/app.py
@@ -32,4 +32,7 @@ def predict_sentiment():
     return jsonify(response)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    # app.run(debug=True, port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+    
