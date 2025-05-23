69b284f3e1cf4b88af401687e9f4313d22681ead
Jesslyn Caroline <158556535+jesslyn-caroline@users.noreply.github.com>
2025-05-23 14:35:38 +0700

    fix: change sentiment value

--- back-end/app.py
+++ back-end/app.py
@@ -1,14 +1,10 @@
-import os
 import joblib
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 # Initialize Flask app
 app = Flask(__name__)
-
-# Enable CORS
 CORS(app)
-
 
 # Load the model and vectorizer
 model = joblib.load("model/model.pkl")
@@ -16,14 +12,14 @@ vectorizer = joblib.load("model/vectorizer.pkl")
 
 # Map the sentiment label
 sentiment = {
-    0: "negative",
-    1: "neutral",
-    2: "positive"
+    0: "Negative Sentiment ☹️",
+    1: "Neutral Sentiment 😐",
+    2: "Positive Sentiment 😊"
 }
 
 @app.route("/")
 def index():
-    return jsonify("API is running.")
+    return "API is running."
 
 @app.route("/api/predict", methods=["POST"])
 def predict_sentiment():
@@ -36,6 +32,4 @@ def predict_sentiment():
     return jsonify(response)
 
 if __name__ == "__main__":
-    # app.run(debug=True, port=8000)
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True, port=8000)
